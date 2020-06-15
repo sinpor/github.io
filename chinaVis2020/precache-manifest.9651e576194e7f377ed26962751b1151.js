41ef580bc2e6dcb172b4e6afa3bf7f5a9650c4b3
邓鑫波 <mymail33333@163.com>
2020-06-15 10:17:14 +0800

    chinavis ui

---
+++ chinaVis2020/precache-manifest.9651e576194e7f377ed26962751b1151.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e7fa26fc42d561806b295a7648ac1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e19e5951e3523ee46e8f",
+    "url": "./static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "f26f88d4b3f1b2242df1",
+    "url": "./static/css/main.cd64bad3.chunk.css"
+  },
+  {
+    "revision": "e19e5951e3523ee46e8f",
+    "url": "./static/js/2.830e9f09.chunk.js"
+  },
+  {
+    "revision": "7eeb0e34c811889e2580cd9f3a03deb1",
+    "url": "./static/js/2.830e9f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26f88d4b3f1b2242df1",
+    "url": "./static/js/main.3a06cd27.chunk.js"
+  },
+  {
+    "revision": "f585165c7f24d23ffb81",
+    "url": "./static/js/runtime-main.f886bc87.js"
+  },
+  {
+    "revision": "8e8e625f12e45ecc674f9579c4547351",
+    "url": "./static/media/DS-DIGII.8e8e625f.TTF"
+  },
+  {
+    "revision": "110420cf1d57846d6bc81289c4a84db9",
+    "url": "./static/media/bg.110420cf.110420cf.png"
+  }
+]);
